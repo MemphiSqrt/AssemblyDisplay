033cd6892f7b0206b94a477a669e7b8426a4e758
Yuanzheng Tao <memphis@pku.edu.cn>
2018-03-25 16:55:00 +0800

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -1,72 +1,52 @@
 	.data
-list:	.asciiz "Alpha ","Bravo ","Charlie ","Delta ","Echo ","Foxtrot ","Golf ","Hotel ","India ","Juliet ","Kilo ","Lima ","Mike ","November ","Oscar ","Papa ","Quebec ","Romeo ","Sierra ","Tango ","Uniform ","Victor ","Whisky ","X-ray ","Yankee ","Zulu "
-index:	.word 0,7,14,23,30,36,45,51,58,65,73,79,85,91,101,108,114,122,129,137,144,153,161,169,176,184	
-number: .asciiz "zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ","eight ","nine " 
-index_n: .word 0,6,11,16,23,29,35,40,47,54   
-name_id:	.asciiz "\r\nname: Tao Yuanzheng. id:1600012799"
+str1:	.asciiz "\r\nSuccess! Location: "
+str2:	.asciiz "\r\nFail!\r\n"
+str3:	.asciiz "\r\n"
+buffer:	.space 100
 
-	.text 
+	.text
 	.globl main
-main:	li $v0, 12
+main:	
+	la $a0, buffer
+	la $a1, 100
+	li $v0, 8
 	syscall
+
+input:  
+	li $v0, 12
+ 	syscall
 	beq $v0, '?', goend
-	
-	sub $t1, $v0, 48
-	slt $s0, $t1, $0
-	bne $s0, $0, others
-	
-	# number
-	sub $t2, $t1, 10
-	slt $s1, $t2, $0
-	bne $s1, $0, getnum
-	
-	# capital
-	sub $t2, $v0, 91
-	slt $s1, $t2, $0
-	sub $t2, $v0, 64
-	sgt $s2, $t2, $0
-	and $s0, $s1, $s2
-	bne $s0, $0, printlist
-	
-	# lower case
-	sub $t2, $v0, 123
-	slt $s1, $t2, $0
-	sub $t2, $v0, 96
-	sgt $s2, $t2, $0
-	and $s0, $s1, $s2
-	bne $s0, $0, printlist
-	
-	j others
+ 	la $s1, buffera
+ 	li $t0, 0
 
-getnum:	sll $t1, $t1, 2
-	la $s0, index_n
-	add $s0, $s0, $t1
-	lw $s1, ($s0)
-	la $a0, number
-	add $a0, $a0, $s1
+loop:  
+	lb $s0, 0($s1)
+	sub $t1, $v0, $s0
+	beq $t1, $0, success
+   	addi $t0, $t0, 1
+	slt $t3, $t0, $a1
+	beq $t3, $0, fail
+	addi $s1 $s1, 1
+	j loop
+
+success:   
+	la $a0, str1
 	li $v0, 4
 	syscall
-	j main
-
-printlist:
-	sub $t2, $t2, 1
-	sll $t2, $t2, 2
-	la $s0, index
-	add $s0, $s0, $t2
-	lw $s1, ($s0)
-	la $a0, list
-	add $a0, $s1, $a0
+	addi $a0, $t0, 1
+ 	li $v0, 1 
+	syscall
+	la $a0, str3
 	li $v0, 4
 	syscall
-	j main
+	j input
 
-others:	li $a0, 42
-	li $v0, 11
-	syscall
-	j main
+fail:       
+	la $a0, str2
+	li $v0, 4
+ 	syscall
+	j input
 
-goend:	li $v0, 4
-	la $a0, name_id
-	syscall
+goend:       
 	li $v0, 10
 	syscall
