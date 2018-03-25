0032f44ad48e0673af7d5c1a31964266b6f3633b
Yuanzheng Tao <memphis@pku.edu.cn>
2018-03-25 21:10:19 +0800

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -7,18 +7,22 @@ buffer:	.space 100
 	.text
 	.globl main
 main:	
+	# read the buffer
 	la $a0, buffer
 	la $a1, 100
 	li $v0, 8
 	syscall
 
+	# read the char
 input:  
 	li $v0, 12
  	syscall
+	# if char = ?, end the program
 	beq $v0, '?', goend
  	la $s1, buffer
  	li $t0, 0
 
+	# enter into the loop and try to find the same char
 loop:  
 	lb $s0, 0($s1)
 	sub $t1, $v0, $s0
@@ -29,6 +33,7 @@ loop:
 	addi $s1 $s1, 1
 	j loop
 
+	# if find successfully
 success:   
 	la $a0, str1
 	li $v0, 4
@@ -41,12 +46,14 @@ success:
 	syscall
 	j input
 
+	# if there's no same char 
 fail:       
 	la $a0, str2
 	li $v0, 4
  	syscall
 	j input
 
+	# end the program
 goend:       
 	li $v0, 10
 	syscall
