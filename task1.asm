066c5d697471bed112c07b01430700b8b935c7df
Yuanzheng Tao <memphis@pku.edu.cn>
2018-03-25 21:04:38 +0800

    Add files via upload

--- task1.asm
+++ task1.asm
@@ -1,5 +1,6 @@
 	.data
 list:	.asciiz "Alpha ","Bravo ","Charlie ","Delta ","Echo ","Foxtrot ","Golf ","Hotel ","India ","Juliet ","Kilo ","Lima ","Mike ","November ","Oscar ","Papa ","Quebec ","Romeo ","Sierra ","Tango ","Uniform ","Victor ","Whisky ","X-ray ","Yankee ","Zulu "
+list2:	.asciiz "alpha ","bravo ","charlie ","delta ","echo ","foxtrot ","golf ","hotel ","india ","juliet ","kilo ","lima ","mike ","november ","oscar ","papa ","quebec ","romeo ","sierra ","tango ","uniform ","victor ","whisky ","x-ray ","yankee ","zulu "
 index:	.word 0,7,14,23,30,36,45,51,58,65,73,79,85,91,101,108,114,122,129,137,144,153,161,169,176,184	
 number: .asciiz "zero ", "one ", "two ", "three ", "four ", "five ", "six ", "seven ","eight ","nine " 
 index_n: 	.word 0,6,11,16,23,29,35,40,47,54   
@@ -33,7 +34,7 @@ main:
 	sub $t2, $v0, 96	
 	sgt $s2, $t2, $0	
 	and $s0, $s1, $s2	
-	bne $s0, $0, printlist	
+	bne $s0, $0, printlist2	
 	j others
 	
 getnum:	
@@ -54,6 +55,18 @@ printlist:
 	add $s0, $s0, $t2	
 	lw $s1, ($s0)	
 	la $a0, list	
+	add $a0, $s1, $a0	
+	li $v0, 4	
+	syscall	
+	j main
+	
+printlist2:	
+	sub $t2, $t2, 1	
+	sll $t2, $t2, 2	
+	la $s0, index	
+	add $s0, $s0, $t2	
+	lw $s1, ($s0)	
+	la $a0, list2	
 	add $a0, $s1, $a0	
 	li $v0, 4	
 	syscall	
